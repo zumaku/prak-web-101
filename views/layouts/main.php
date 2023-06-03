fb16e4b27a29881b2066e2f5908fe2387e4908a4
Zumaku <60200121101@uin-alauddin.ac.id>
2023-06-04 07:35:47 +0800

    Menghapus navbar kecuali home & menambah mahasiswa

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,18 +40,19 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
         'options' => ['class' => 'navbar-nav'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>'
+            ['label' => 'Mahasiswa', 'url' => ['/site/mahasiswa']],
+            // ['label' => 'About', 'url' => ['/site/about']],
+            // ['label' => 'Contact', 'url' => ['/site/contact']],
+            // Yii::$app->user->isGuest
+            //     ? ['label' => 'Login', 'url' => ['/site/login']]
+            //     : '<li class="nav-item">'
+            //         . Html::beginForm(['/site/logout'])
+            //         . Html::submitButton(
+            //             'Logout (' . Yii::$app->user->identity->username . ')',
+            //             ['class' => 'nav-link btn btn-link logout']
+            //         )
+            //         . Html::endForm()
+            //         . '</li>'
         ]
     ]);
     NavBar::end();
