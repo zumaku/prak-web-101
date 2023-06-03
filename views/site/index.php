eea6f6a1c6be1b1b3bdc89e6e6d4752b03317756
Zumaku <60200121101@uin-alauddin.ac.id>
2023-06-04 07:41:35 +0800

    Mengupdate tampilan home

--- views/site/index.php
+++ views/site/index.php
@@ -6,46 +6,25 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    <div class="jumbotron text-center bg-transparent mt-1 mb-1">
+        <h1 class="display-4">Welcome Zul Fadli</h1>
+        <p class="lead">Berikut Aturan Laporan Praktikum 3</p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <h2>Aturan 1</h2>
+                <p>Silahkan gunakan project yang sudah ada, kemudian buat tampilan dan proses seperti yang ada pada menu Mahasiswa, setiap kolom pada table wajib mencantumkan 3 digit terakhir nim kalian, setiap record wajib mencantumkan NIM kalian yang di akhiri dengan 3 digit angka ramdom (seperti pada menu mahasiswa)</p>
             </div>
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <h2>Aturan 2</h2>
+                <p>Silahkan buat tampilannya persis sama seperti yang ada pada menu Home, dan Mahasiswa, setiap komponen yang tidak sesuai akan mengurangi 5 poin dari total 20 point Keaktifan</p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <h2>Aturan 3</h2>
+                <p>Silahkan Post Kedalam github Project karena masuk dalam penilaian Penguasaan Software</p>
             </div>
         </div>
 
