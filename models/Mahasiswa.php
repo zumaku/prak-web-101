752ecf3bea1f4d454661bc91596037a0c72aaa75
Zumaku <60200121101@uin-alauddin.ac.id>
2023-06-04 08:25:38 +0800

    Membuat model mahasiswa lapran 3

---
+++ models/Mahasiswa.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "mahasiswa".
+ *
+ * @property int $id_101
+ * @property string|null $no_induk_mahasiswa_101
+ * @property string|null $nama_mahasiswa_101
+ * @property string|null $kelas_101
+ * @property string|null $status_101
+ */
+class Mahasiswa extends \yii\db\ActiveRecord
+{
+    public static function tableName(){
+        return 'mahasiswa';
+    }
+
+    public function rules(){
+        return [
+            [['no_induk_mahasiswa_101', 'nama_mahasiswa_101', 'kelas_101', 'status_101'], 'string', 'max' => 255],
+        ];
+    }
+
+    public function attributeLabels(){
+        return [
+            'id_101' => 'ID 101',
+            'no_induk_mahasiswa_101' => 'No. Induk Mahasiswa 101',
+            'nama_mahasiswa_101' => 'Nama Mahasiswa 101',
+            'kelas_101' => 'Kelas 101',
+            'status_101' => 'Status 101',
+        ];
+    }
+}
