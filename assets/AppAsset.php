ad945db626351f10317cd017db58fbc039ebca3c
Fadli in Love <zulfadli15903@gmail.com>
2023-06-09 09:00:20 +0800

    include css and js files

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,8 +21,33 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        "template/css/bootstrap.min.css",
+        "template/css/core.css",
+        "template/css/components.css",
+        "template/css/icons.css",
+        "template/css/pages.css",
+        "template/css/responsive.css",
     ];
     public $js = [
+        "assets/js/jquery.min.js",
+        "assets/js/bootstrap.min.js",
+        "assets/js/detect.js",
+        "assets/js/fastclick.js",
+        "assets/js/jquery.slimscroll.js",
+        "assets/js/jquery.blockUI.js",
+        "assets/js/waves.js",
+        "assets/js/wow.min.js",
+        "assets/js/jquery.nicescroll.js",
+        "assets/js/jquery.scrollTo.min.js",
+        "assets/plugins/peity/jquery.peity.min.js",
+        "assets/plugins/waypoints/lib/jquery.waypoints.js",
+        "assets/plugins/counterup/jquery.counterup.min.js",
+        "assets/plugins/morris/morris.min.js",
+        "assets/plugins/raphael/raphael-min.js",
+        "assets/plugins/jquery-knob/jquery.knob.js",
+        "assets/pages/jquery.dashboard.js",
+        "assets/js/jquery.core.js",
+        "assets/js/jquery.app.js",
     ];
     public $depends = [
         'yii\web\YiiAsset',
