65fb9babd29273908373424f6f4ce675afbae4da
Fadli in Love <zulfadli15903@gmail.com>
2023-06-09 09:04:54 +0800

    config asset (css & js) manager

--- config/web.php
+++ config/web.php
@@ -51,6 +51,28 @@ $config = [
             'rules' => [
             ],
         ],
+
+        // aset manager
+        'assetManager' => [
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'sourcePath' => null,
+                    'basePath' => @webroot,
+                    'baseUrl' => @web,
+                    'js' => [
+                        'template/js/jquery.min.css'
+                    ],
+                ],
+                'yii\bootstrap5\BootstrapAsset' => [
+                    'sourcePath' => null,
+                    'basePath' => @webroot,
+                    'baseUrl' => @web,
+                    'css' => [
+                        'template/css/bootstrap.min.css'
+                    ],
+                ],
+            ],
+        ]
         
     ],
     'params' => $params,
